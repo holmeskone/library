622303a79eb639c018fa6d1eacb183f02131d3b9
holmeskone <165698602+holmeskone@users.noreply.github.com>
2024-10-02 12:28:13 +0100

    Add table when adding a new book, with the titles of the library displayed in the first column

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ function Book() {
 }
 
 
+
 //Add book to myLibrary array and print it
 function addBookToLibrary() {
   // do stuff here
@@ -26,25 +27,46 @@ document.querySelector("form.add-book").addEventListener("submit", function(even
 });
 
 //Display value in a table
-
 function createTable(){
-    // creates a <table> element and a <tbody> element
+    // creates a <table> element, a <thead> and a <tbody> element
     const tbl = document.createElement("table");
+    const thead = document.createElement("thead");
     const tblBody = document.createElement("tbody");
-    const thead = document.createElement("th");
 
+    //creates a header for the table
+    const headerRow = document.createElement("tr");
+
+    //Titles of each header in the table
+    const headers = ['Title', 'Author', 'Number of Pages', 'Status', 'Edit', 'Remove']
+
+    // Create 7 columns for the header
+    for (let i = 0; i < headers.length; i++) {
+        const headerCell = document.createElement("th"); // each cell of in the table column
+        const headerText = document.createTextNode(headers[i]);
+        headerCell.appendChild(headerText); // Add the text to the table header
+        headerRow.appendChild(headerCell); // Add each table header to the table row
+    }
+
+    //Adds header table row to the table head
+    thead.appendChild(headerRow)
+
+    //Creates the rows of the table corresponding to the books
     for (i=0; i<myLibrary.length; i++){
         const row = document.createElement("tr");
-        for(j=0; j<7; j++){
+        for(j=0; j<6; j++){
             const cell = document.createElement("td");
-            const cellText = document.createTextNode(`cell in row ${i}, column ${j}`);
+            if(j===0){
+            const cellText = document.createTextNode(myLibrary[i]);
             cell.appendChild(cellText);
+        }
             row.appendChild(cell);
         }
-        row.appendChild(thead);
         tblBody.appendChild(row)    
     }
-    tbl.appendChild(tblBody);
+    tbl.appendChild(thead); //Add the thead to the table
+    tbl.appendChild(tblBody); // Add the table body to the table
     document.body.appendChild(tbl);
 
 }
+
+//Add pop-up when adding a book
