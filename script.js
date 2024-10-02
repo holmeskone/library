185476e284016c0deecb47f9aa4590ffebe955a0
holmeskone <165698602+holmeskone@users.noreply.github.com>
2024-10-02 11:25:16 +0100

    Add capability to create a table when add book button is clicked

--- script.js
+++ script.js
@@ -1,23 +1,50 @@
-const inputBook = document.getElementById("add-book");
+const inputBook = document.getElementById("book-name");
 const submitBook = document.getElementById("submit-button");
 
 
-const myLibrary = [];
+const myLibrary = ["HarryPotter", "LoR", "Jumanji"];
 
 function Book() {
   // the constructor...
+
 }
 
+
+//Add book to myLibrary array and print it
 function addBookToLibrary() {
   // do stuff here
-    addBook = inputBook.value;
-    myLibrary.push(addBook);
-    console.log(myLibrary);
-    return false;
+    addBook = inputBook.value; // Gets the inputed value 
+    myLibrary.push(addBook); // add input to array
+    console.log(myLibrary); // print array
+    return false;  // Make the page not refresh when the form is submitted
 };
 
-//   if(submitBook === true){
-//     myLibrary.push(book);
-//   }
-//   console.log(myLibrary)
-// }
+// Display value of inputted book 
+document.querySelector("form.add-book").addEventListener("submit", function(event) { // Trigger this function when the submit type has been clicked
+    event.preventDefault(); // Make the page not refresh when the form is submitted
+    document.getElementById("display").innerText = document.getElementById("book-name").value; // add value of book-name (input) into the div 'display'
+});
+
+//Display value in a table
+
+function createTable(){
+    // creates a <table> element and a <tbody> element
+    const tbl = document.createElement("table");
+    const tblBody = document.createElement("tbody");
+    const thead = document.createElement("th");
+
+    for (i=0; i<myLibrary.length; i++){
+        const row = document.createElement("tr");
+        for(j=0; j<7; j++){
+            const cell = document.createElement("td");
+            const cellText = document.createTextNode(`cell in row ${i}, column ${j}`);
+            cell.appendChild(cellText);
+            row.appendChild(cell);
+        }
+        row.appendChild(thead);
+        tblBody.appendChild(row)    
+    }
+    tbl.appendChild(tblBody);
+    document.body.appendChild(tbl);
+
+}
